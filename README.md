2780c079e17c563ac95eef8d3bdf1bec3ab02747
vincentgosselin1 <vincentgosselin1@gmail.com>
2016-07-22 22:46:52 -0400

    file separation atm

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # RubyEmailBotV2
 Now with a User Interface using the JVM from Java.
 Almost there, all the XML stuff is now replaced with an interactive console.
+
+##TODO 
+	[ ] Separation of files for MVC. View to debug!
--- view.rb
+++ view.rb
@@ -18,99 +18,114 @@ puts "Welcome to Ruby Email Bot, By Vincent Gosselin"
 sleep(5)
 system "clear"
 
-class Example < JFrame
-  
+class View < JFrame
+
+    #Buttons
+    JButton cvButton
+    JButton clButton
+    JButton configButton 
+    JButton emailListButton 
+
+
     def initialize
         super "Ruby Email Bot"
         
         self.initUI
     end
+
+    def addButtonListener(buttonListener)
+        cvButton.addActionListener(buttonListener)
+    end
+
       
     def initUI
       
         @panel = JPanel.new
         @panel.setLayout GridLayout.new 2, 2
 
-        #CV Button.
         cvButton = JButton.new "Open CV"
-        cvButton.addActionListener do |e|
-            chooseFile = JFileChooser.new
-            filter = FileNameExtensionFilter.new "pdf files", "pdf"
-            chooseFile.addChoosableFileFilter filter
-            ret = chooseFile.showDialog @panel, "Choose file"
-            if ret == JFileChooser::APPROVE_OPTION
-                file = chooseFile.getSelectedFile
-                puts "We got your CV : " + file.getName
-                sleep(5)
-                system "clear"   
-            end    
-        end
+        clButton = JButton.new "Open a Cover Letter"
+        configButton = JButton.new "Configure your Email"
+        emailListButton = JButton.new "Email List"
+
+        #CV Button.
+        #cvButton = JButton.new "Open CV"
+        # cvButton.addActionListener do |e|
+        #     chooseFile = JFileChooser.new
+        #     filter = FileNameExtensionFilter.new "pdf files", "pdf"
+        #     chooseFile.addChoosableFileFilter filter
+        #     ret = chooseFile.showDialog @panel, "Choose file"
+        #     if ret == JFileChooser::APPROVE_OPTION
+        #         file = chooseFile.getSelectedFile
+        #         puts "We got your CV : " + file.getName
+        #         sleep(5)
+        #         system "clear"   
+        #     end    
+        # end
 
         #CoverLetter Button.
-        clButton = JButton.new "Open a Cover Letter"
-        clButton.addActionListener do |e|
-            chooseFile = JFileChooser.new
-            filter = FileNameExtensionFilter.new "pdf files", "pdf"
-            chooseFile.addChoosableFileFilter filter
-            ret = chooseFile.showDialog @panel, "Choose file"
-            if ret == JFileChooser::APPROVE_OPTION
-                file = chooseFile.getSelectedFile
-                puts "We got your Cover Letter: " + file.getName
-                sleep(5)
-                system "clear"      
-            end    
-        end
+        #clButton = JButton.new "Open a Cover Letter"
+        # clButton.addActionListener do |e|
+        #     chooseFile = JFileChooser.new
+        #     filter = FileNameExtensionFilter.new "pdf files", "pdf"
+        #     chooseFile.addChoosableFileFilter filter
+        #     ret = chooseFile.showDialog @panel, "Choose file"
+        #     if ret == JFileChooser::APPROVE_OPTION
+        #         file = chooseFile.getSelectedFile
+        #         puts "We got your Cover Letter: " + file.getName
+        #         sleep(5)
+        #         system "clear"      
+        #     end    
+        # end
         
-        configButton = JButton.new "Configure your Email"
-        configButton.addActionListener do |e|
-        validation = 0 #To validate user input.    
-            while validation==0 do
-                puts "Enter your email address: "
-                @emailAddress = gets.chomp
-                puts @emailAddress
-                puts "Now, enter your password : "
-                @emailPassword = gets.chomp
-                puts @emailPassword
-                puts "Do you confirm? Yn"
-                i = gets.chomp
-                if i == "Y"
-                    validation = 1  #We're done here.
-                end
-            end
-        puts "Email configured"
-        sleep(5)
-        system "clear"         
-        end
+        #configButton = JButton.new "Configure your Email"
+        # configButton.addActionListener do |e|
+        # validation = 0 #To validate user input.    
+        #     while validation==0 do
+        #         puts "Enter your email address: "
+        #         @emailAddress = gets.chomp
+        #         puts @emailAddress
+        #         puts "Now, enter your password : "
+        #         @emailPassword = gets.chomp
+        #         puts @emailPassword
+        #         puts "Do you confirm? Yn"
+        #         i = gets.chomp
+        #         if i == "Y"
+        #             validation = 1  #We're done here.
+        #         end
+        #     end
+        # puts "Email configured"
+        # sleep(5)
+        # system "clear"         
+        # end
         
-        emailListButton = JButton.new "Email List"
-        emailListButton.addActionListener do |e|
-            ##How many Emails?
-            validation = 0
-            while validation == 0 do
-                puts "How many Emails you want to send?"
-                i = gets.chomp
-                    if i.to_i < MAXEMAIL_CONST
-                        validation = 1
-                        puts "great"
-                    else
-                    puts "WARNING! You can only send " + MAXEMAIL_CONST.to_s + " emails "
-                    end
-            end        
-            puts "We are going to send " + i.to_s + " Emails"
-            j = 1 #counter for entering email addresses.
-            list = Array.new(i.to_i)
-            until j.to_i == i.to_i+1 do
-                puts "Enter company Email #" + j.to_s + " Right now : "
-                list[j.to_i-1] = gets.chomp
-                puts "                        -----> " + list[j.to_i-1].to_s
-                j+=1
-            end
-            puts "Email List Configured"
-            sleep(5)
-            system "clear"     
-        end
-
-
+        #emailListButton = JButton.new "Email List"
+        # emailListButton.addActionListener do |e|
+        #     ##How many Emails?
+        #     validation = 0
+        #     while validation == 0 do
+        #         puts "How many Emails you want to send?"
+        #         i = gets.chomp
+        #             if i.to_i < MAXEMAIL_CONST
+        #                 validation = 1
+        #                 puts "great"
+        #             else
+        #             puts "WARNING! You can only send " + MAXEMAIL_CONST.to_s + " emails "
+        #             end
+        #     end        
+        #     puts "We are going to send " + i.to_s + " Emails"
+        #     j = 1 #counter for entering email addresses.
+        #     list = Array.new(i.to_i)
+        #     until j.to_i == i.to_i+1 do
+        #         puts "Enter company Email #" + j.to_s + " Right now : "
+        #         list[j.to_i-1] = gets.chomp
+        #         puts "                        -----> " + list[j.to_i-1].to_s
+        #         j+=1
+        #     end
+        #     puts "Email List Configured"
+        #     sleep(5)
+        #     system "clear"     
+        # end
             
     
         @panel.add cvButton
@@ -128,4 +143,4 @@ class Example < JFrame
     end
 end
 
-Example.new
+View.new
